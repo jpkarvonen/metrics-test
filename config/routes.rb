2854055b49cf409c03bb8a471999e5ddb042a1cb
John Karvonen <jpkarvonen@gmail.com>
2018-12-14 12:57:09 -0500

    create API controller

--- config/routes.rb
+++ config/routes.rb
@@ -5,4 +5,9 @@ Rails.application.routes.draw do
 
   get 'welcome/about'
   root 'registered_applications#index'
+
+  namespace :api, defaults: {format: :json } do
+    match '/events', to: 'events#preflight', via: [:options]
+    resources :events, only: [:create]
+  end
 end
